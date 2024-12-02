72054c6707002d89370107bd1013ca3edee426f0
slayvict <shashkov.yaroslav@crypton.lan>
2024-12-02 12:26:21 +0200

    I hope

--- day-two/part-two/src/main.rs
+++ day-two/part-two/src/main.rs
@@ -40,7 +40,7 @@ fn get_trend(report: &[i32]) -> Trend {
     }
 }
 
-fn is_safe_report(report: &[i32], min_change: i32, max_change: i32, tolerance: i32) -> bool {
+fn is_safe_report(report: &[i32], min_change: i32, max_change: i32) -> bool {
     let trend = get_trend(report);
     let mut anomalies_count = 0;
 
@@ -51,34 +51,32 @@ fn is_safe_report(report: &[i32], min_change: i32, max_change: i32, tolerance: i
             Trend::Chaos => false,
         };
         ((level1.abs_diff(level2) as i32) >= min_change
-            || (level1.abs_diff(level2) as i32) <= max_change)
+            && (level1.abs_diff(level2) as i32) <= max_change)
             && compare_trend
     };
 
-    let mut i = 1;
-    while i < report.len() {
-        let regular = compare_levels(report[i - 1], report[i]);
-        let anomaly = i < report.len() - 1 && compare_levels(report[i - 1], report[i + 1]);
+    let mut i = 0;
+    while i < report.len() - 1 {
+        let regular = compare_levels(report[i], report[i + 1]);
+        let skip = i < report.len() - 2 && compare_levels(report[i], report[i + 2]);
+        let future = (i < report.len() - 2 && compare_levels(report[i + 1], report[i + 2]))
+            || i >= report.len() - 2;
+        let future_skip = (i < report.len() - 3 && compare_levels(report[i + 1], report[i + 3]))
+            || i >= report.len() - 3;
 
-        match (regular, anomaly) {
-            (true, true) => {
-                if !compare_levels(report[i], report[i + 1]) {
-                    anomalies_count += 1;
-                    // i += 1;
-                }
-            }
-            (true, false) => (),
-            (false, true) => {
-                anomalies_count += 1;
+        // println!("{i}: {regular}, {skip}, {future}, {future_skip}");
+        match (regular, skip, future, future_skip) {
+            (_, false, false, _) => return false,
+            (_, true, false, _) => {
                 i += 1;
+                anomalies_count += 1
             }
-            (false, false) => anomalies_count += 1,
+            _ => (),
         }
-
-        i += 1;
+        i += 1
     }
 
-    if anomalies_count > tolerance {
+    if anomalies_count > 1 {
         false
     } else {
         true
@@ -96,7 +94,7 @@ fn main() {
 
     let result = reports
         .iter()
-        .filter(|report| is_safe_report(report, args.min_change, args.max_change, 1))
+        .filter(|report| is_safe_report(report, args.min_change, args.max_change))
         .count();
 
     println!("result: {result}");
@@ -109,41 +107,41 @@ mod tests {
     #[test]
     fn test76421safe() {
         let report = vec![7, 6, 4, 2, 1];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test12789unsafe() {
         let report = vec![1, 2, 7, 8, 9];
-        assert!(!is_safe_report(&report, 1, 3, 1));
+        assert!(!is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test97621unsafe() {
         let report = vec![9, 7, 6, 2, 1];
-        assert!(!is_safe_report(&report, 1, 3, 1));
+        assert!(!is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test13245safe() {
         let report = vec![1, 3, 2, 4, 5];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test86441safe() {
         let report = vec![8, 6, 4, 4, 1];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test13679safe() {
         let report = vec![1, 3, 6, 7, 9];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test93679safe() {
         let report = vec![9, 3, 6, 7, 9];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
     #[test]
     fn test16421safe() {
         let report = vec![1, 6, 4, 2, 1];
-        assert!(is_safe_report(&report, 1, 3, 1));
+        assert!(is_safe_report(&report, 1, 3));
     }
 }
