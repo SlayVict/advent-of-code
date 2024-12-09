569c1e83181f8f5e5c786baec0bdd871d7ef2ddc
SlayVict <superjamperua@gmail.com>
2024-12-09 21:13:01 +0000

    day09 part2 done

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -1,7 +1,16 @@
+use std::{cmp::Reverse, collections::BinaryHeap, error::Error};
+
 use crate::utils::answers::Answer;
 
 pub fn part1(input: &str) -> Answer {
-    let mut input = parse(input);
+    let mut input: Vec<_> = input
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect();
+
     let mut index: u64 = 0;
     let mut left: u64 = 0;
     let mut left_index = 0 as usize;
@@ -10,22 +19,6 @@ pub fn part1(input: &str) -> Answer {
     let mut sum: u64 = 0;
 
     let mut accounted = 0;
-
-    // while left < right {
-    //     if let Some(l) = input[left as usize] {
-    //         sum += l as u64 * left;
-    //         // print!("{l} ");
-    //         left += 1;
-    //     } else if let Some(r) = input[right as usize - 1] {
-    //         right -= 1;
-    //         sum += r as u64 * left;
-    //         // print!("{r} ");
-    //         left += 1;
-    //     } else {
-    //         right -= 1;
-    //     }
-    // }
-    // println!();
 
     while left_index <= right_index {
         if left_index % 2 == 0 {
@@ -87,102 +80,224 @@ fn print_drive(input: &Vec<DriveSpace>) {
     println!()
 }
 
-pub fn part2(input: &str) -> Answer {
-    let mut input = parse_2(input);
-    let size = input.len();
-    let mut moved = Vec::new();
+#[derive(Debug, Clone)]
+struct SizeError;
 
-    let mut prev_index = 0;
-    loop {
-        let moved_count = moved.len();
-        println!("{moved_count}/{size}");
-        let first = input
-            .iter()
-            .enumerate()
-            .rev()
-            .filter(|(_, &item)| {
-                if let DriveSpace::File((width, id)) = item {
-                    !moved.contains(&id)
-                } else {
-                    false
-                }
-            })
-            .next();
-        let Some((file_index, &file)) = first else {
-            break;
-        };
-        let DriveSpace::File((file_width, id)) = file else {
-            panic!()
-        };
-        moved.push(id);
+// impl Error for SizeError {}
 
-        let first_free = input[..file_index]
-            .iter()
-            .enumerate()
-            .filter(|(_, &space)| match space {
-                DriveSpace::File(_) => false,
-                DriveSpace::Free(free_width) => free_width >= file_width,
-            })
-            .next();
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct File {
+    start: u64, // inclusive
+    end: u64,   // inclusive
+    id: u64,
+}
 
-        if let Some((free_index, &free_space)) = first_free {
-            if free_space.width() > file_width {
-                input[free_index] = DriveSpace::Free(free_space.width() - file_width);
-            } else {
-                input.remove(free_index);
-            }
-            let tmp = input.iter().position(|i| *i == file).unwrap();
-            input.remove(tmp);
-            input.insert(tmp, DriveSpace::Free(file.width()));
+impl File {
+    fn new(start: u64, end: u64, id: u64) -> Result<Self, SizeError> {
+        if start <= end {
+            Ok(Self { start, end, id })
+        } else {
+            Err(SizeError)
+        }
+    }
+    fn width(&self) -> u64 {
+        self.end - self.start + 1
+    }
+    fn checksum(&self) -> u64 {
+        (self.start..=self.end).sum::<u64>() * self.id
+    }
+}
 
-            input.insert(free_index, file);
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Free {
+    start: u64,
+    end: u64,
+}
+
+impl Free {
+    fn new(start: u64, end: u64) -> Result<Self, SizeError> {
+        if start <= end {
+            Ok(Self { start, end })
+        } else {
+            Err(SizeError)
         }
     }
 
-    // print_drive(&input);
-    let mut sum = 0u64;
-    let mut index = 0u64;
-    for space in input {
-        let c = match space {
-            DriveSpace::File((_, id)) => id,
-            DriveSpace::Free(_) => 0,
-        };
-        sum += (0..space.width())
-            .map(|i| (c as u64 * (index + i as u64)) as u64)
-            .sum::<u64>();
-        index += space.width() as u64;
+    fn width(&self) -> u64 {
+        self.end - self.start + 1
+    }
+}
+
+impl Ord for Free {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.start.cmp(&other.start)
+    }
+}
+
+impl PartialOrd for Free {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.start.cmp(&other.start))
+    }
+}
+
+#[derive(Debug)]
+struct Drive {
+    files: Vec<File>,
+    free: [BinaryHeap<Reverse<Free>>; 9],
+}
+
+fn display_files(files: &Vec<File>) {
+    let mut files = files.clone();
+    files.sort_by_key(|&f| f.start);
+    let mut file_index = 0usize;
+    let mut index = 0usize;
+
+    let mut id = 0;
+    while file_index < files.len() {
+        if index <= files[file_index].end as usize && index >= files[file_index].start as usize {
+            id = files[file_index].id;
+            print!("{id}");
+        } else {
+            print!(".");
+        }
+        index += 1;
+        if index > files[file_index].end as usize {
+            file_index += 1;
+        }
+    }
+    println!()
+}
+
+pub fn part2(input: &str) -> Answer {
+    /* target 6347435485773 */
+    let mut drive = parse(input);
+
+    // display_files(&drive.files);
+
+    for i in (0..drive.files.len()).rev() {
+        // println!("{:?}", drive.free);
+        let min = drive
+            .free
+            .iter()
+            .map(|heap| heap.peek())
+            .filter(|peek| peek.is_some())
+            .map(|peek| peek.unwrap().0)
+            .min();
+
+        // if let None = min {
+        //     break;
+        // }
+
+        let mut file = drive.files[i];
+        let width = file.width();
+
+        // if file.start < min.unwrap().start {
+        //     // println!("break bekause {} < {}", file.start, min.unwrap().start);
+        //     continue;
+        // }
+
+        let free_space = drive
+            .free
+            .iter()
+            .enumerate()
+            .skip(width as usize - 1)
+            .map(|(i, heap)| (i + 1, heap.peek()))
+            .filter(|(_, peek)| peek.is_some())
+            .map(|(i, peek)| (i, peek.unwrap()))
+            .min_by_key(|(_, &peek)| (peek).0.start);
+
+        if let Some((free_width, _)) = free_space {
+            let id = file.id;
+            let free = drive.free[free_width - 1].pop().unwrap().0;
+            if free.start > file.start {
+                continue;
+            }
+            // println!("{id}: {width} {free:?}\t\t {free_width}");
+            file.start = free.start;
+            file.end = file.start + width - 1;
+            if width < (free_width as u64) {
+                let new_free = Free::new(file.end + 1, free.end).unwrap();
+                drive.free[new_free.width() as usize - 1].push(Reverse(new_free));
+            }
+            drive.files[i] = file;
+        }
     }
 
-    sum.into()
+    // display_files(&drive.files);
+    drive
+        .files
+        .iter()
+        .map(|&f| f.checksum())
+        .sum::<u64>()
+        .into()
 }
 
-fn parse(input: &str) -> Vec<u8> {
-    input
+fn parse(input: &str) -> Drive {
+    let mut free = [
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+        BinaryHeap::new(),
+    ];
+    let mut files = Vec::new();
+    let iter = input
         .lines()
         .next()
         .unwrap()
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as u8)
-        .collect()
-}
+        .map(|c| c.to_digit(10).unwrap() as u8);
 
-fn parse_2(input: &str) -> Vec<DriveSpace> {
-    input
-        .lines()
-        .next()
-        .unwrap()
-        .chars()
-        .enumerate()
-        .map(|(i, c)| {
-            let width = c.to_digit(10).unwrap();
-            match i % 2 {
-                0 => DriveSpace::File((width, i as u32 / 2)),
-                _ => DriveSpace::Free(width),
+    let mut index = 0;
+    for (i, width) in iter.enumerate() {
+        if width > 0 {
+            if i % 2 == 0 {
+                files.push(File::new(index, index + width as u64 - 1, i as u64 / 2).unwrap());
+            } else {
+                free[width as usize - 1]
+                    .push(Reverse(Free::new(index, index + width as u64 - 1).unwrap()));
             }
-        })
-        .filter(|space| match space {
-            DriveSpace::File((width, _)) => *width > 0,
-            DriveSpace::Free(width) => *width > 0,
-        })
-        .collect()
+        }
+        index += width as u64;
+    }
+
+    Drive { files, free }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::cmp::Reverse;
+
+    use super::*;
+
+    const INPUT: &str = "2333133121414131402";
+    #[test]
+    fn test_heap() {
+        let mut heap = BinaryHeap::new();
+        heap.push(Reverse(Free::new(1, 5).unwrap()));
+        heap.push(Reverse(Free::new(10, 15).unwrap()));
+        heap.push(Reverse(Free::new(5, 8).unwrap()));
+
+        assert_eq!(*heap.peek().unwrap(), Reverse(Free::new(1, 5).unwrap()));
+    }
+
+    #[test]
+    fn test_min_viable() {
+        let drive = parse(&INPUT);
+
+        let min = drive
+            .free
+            .iter()
+            .map(|heap| heap.peek())
+            .filter(|peek| peek.is_some())
+            .map(|peek| peek.unwrap().0)
+            .min();
+        let (Some(min)) = min else { panic!() };
+        assert_eq!(min, Free::new(2, 4).unwrap())
+    }
 }
