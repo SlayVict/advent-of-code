eb20ee5f60d04cfe998ce24fa9e5a1401298f83c
SlayVict <superjamperua@gmail.com>
2025-12-05 21:56:13 +0100

    optimal day 3

--- src/year2025/day03.rs
+++ src/year2025/day03.rs
@@ -1,47 +1,42 @@
+use std::mem::replace;
+
 use crate::utils::{answers::Answer, grid::Grid, parse::ParseByte, point::Point};
 
 pub fn part1(input: &str) -> Answer {
-    let grid = parse(input);
-
-    let mut result = 0;
-
-    for i in 0..grid.height {
-        let width = grid.width;
-
-        let mut buff: Vec<[u8; 2]> = vec![[0, 0]; (width - 1) as usize];
-
-        for j in 0..width - 1 {
-            let point = Point::new(j, i);
-            let jolt = grid[point].to_decimal();
-
-            buff[j as usize][0] = buff[(j - 1).max(0) as usize][0].max(jolt);
-        }
-
-        let last = Point::new(width - 1, i);
-        let jolt = grid[last].to_decimal();
-        buff[width as usize - 2][1] = jolt;
-
-        for j in (1..width - 1).rev() {
-            let point = Point::new(j, i);
-            let jolt = grid[point].to_decimal();
-
-            buff[j as usize - 1][1] = buff[(j).min(width - 1) as usize][1].max(jolt);
-        }
-
-        result += buff
-            .iter()
-            .map(|pair| pair[0] as u32 * 10 + pair[1] as u32)
-            .max()
-            .unwrap();
-    }
-
-    result.into()
+    let input = parse(input);
+    solve::<2>(&input).into()
 }
 
 pub fn part2(input: &str) -> Answer {
-    Answer::InProgress
+    let input = parse(input);
+    solve::<12>(&input).into()
 }
 
-fn parse(input: &str) -> Grid<u8> {
-    Grid::parse(input)
+pub fn parse(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+fn solve<const N: usize>(input: &[&str]) -> u64 {
+    let mut batteries = [0; N];
+
+    let mut result = 0;
+
+    for &bank in input {
+        let left = bank.len() - N;
+        batteries.copy_from_slice(&bank.as_bytes()[left..]);
+
+        for mut next in bank[..left].bytes().rev() {
+            for battery in &mut batteries {
+                if next < *battery {
+                    break;
+                }
+                next = replace(battery, next);
+            }
+        }
+        result += batteries
+            .iter()
+            .fold(0, |joltage, &b| 10 * joltage + (b - b'0') as u64)
+    }
+
+    result
 }
